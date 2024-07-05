85450419fe77a045969f6dc720f623c141590bc0
Corentin Forler <corentin@dokos.io>
2024-07-05 16:48:36 +0200

    feat(PWA): Add translations to bootinfo

--- hrms/www/hrms.py
+++ hrms/www/hrms.py
@@ -1,4 +1,5 @@
 import frappe
+from frappe.boot import load_translations
 
 no_cache = 1
 
@@ -20,4 +21,13 @@ def get_context_for_dev():
 
 
 def get_boot():
-	return frappe._dict({"push_relay_server_url": frappe.conf.get("push_relay_server_url")})
+	bootinfo = frappe._dict(
+		{
+			"push_relay_server_url": frappe.conf.get("push_relay_server_url") or "",
+		}
+	)
+
+	bootinfo.lang = frappe.local.lang
+	load_translations(bootinfo)
+
+	return bootinfo
